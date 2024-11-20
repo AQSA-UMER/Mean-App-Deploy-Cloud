a9199b321f5f10f928540f5430a1a734803f73c4
AQSA-UMER <umeraqsa20118@gmail.com>
2024-11-20 19:56:43 +0500

    update pakcage.json file

--- Mocha/restapi-testing/test/global-setup.mjs
+++ Mocha/restapi-testing/test/global-setup.mjs
@@ -4,8 +4,10 @@ import mongoose from 'mongoose';
 
 export async function mochaGlobalSetup(){
     console.log("Global Setup: connecting to database - global setup");
+    // use environment variable for mongoDB connection URI
+    const mongoUri = process.env.MONGO_URL || 'mongodb://localhost:27017/testDB'
 
-    await mongoose.connect('mongodb://localhost:27017/testDB');
+    await mongoose.connect(mongoUri);
 
     //global state or shared state
     global.globalData = {count:0}
