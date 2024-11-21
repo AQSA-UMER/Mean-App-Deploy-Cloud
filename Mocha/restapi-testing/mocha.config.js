dd171090bd0ab83f85f3e34b978cb485efc3fee3
AQSA-UMER <umeraqsa20118@gmail.com>
2024-11-21 21:02:02 +0500

    updated files

--- Mocha/restapi-testing/mocha.config.js
+++ Mocha/restapi-testing/mocha.config.js
@@ -1,4 +1,22 @@
-module.exports = {
+// module.exports = {
+//     reporter: 'mochawesome',
+//     require:['./test/global-setup.mjs']
+// }
+
+/*module.exports = {
     reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: './mochawesome-report', // Directory where the report will be generated
+    reportFilename: 'results', // Name of the report file
+    inlineAssets: true, // Include assets in the report
+  },
+    require:['./test/global-setup.mjs']
+}*/
+
+module.exports = {
+    reporter: 'mocha-junit-reporter',
+    reporterOptions: {
+        mochaFile: './test-results.xml'
+    },
     require:['./test/global-setup.mjs']
 }
